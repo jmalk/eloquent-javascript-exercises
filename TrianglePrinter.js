6b87303870215cae68b786e436cd899c16bccb8d
Josh Malkinson <josh.malkinson@gmail.com>
2015-04-03 11:52:44 +0100

    TrianglePrinter: log a triangle of hashes to the console

---
+++ TrianglePrinter.js
@@ -0,0 +1,17 @@
+/* 
+ * Eloquent JavaScript, Chapter 2 Ex 1
+ *
+ * Write a loop that makes seven calls to console.log to output a triangle of hashes (seven hashes on the bottom row, one fewer on each row up).
+ */
+
+var hash = "#";
+var lineOfHashes = "";
+var baseWidthOfTriangle = 7;
+
+for (rowLength = 1; rowLength <= baseWidthOfTriangle; rowLength++) {
+    while (lineOfHashes.length < rowLength) {
+        lineOfHashes += hash;
+    }
+    console.log(lineOfHashes);
+    lineOfHashes = "";
+}
