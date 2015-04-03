aa91e1a60d3d5e4009a48a805ae722b1f2bd13f6
Josh Malkinson <josh.malkinson@gmail.com>
2015-04-03 11:57:30 +0100

    ChessBoardPrinter: state problem in a comment.

---
+++ ChessBoardPrinter.js
@@ -0,0 +1,9 @@
+/* 
+ * Eloquent JavaScript, Chapter 2 Ex 3
+ *
+ * Write a program that creates a string that represents an 8x8 grid, using
+ * newline characters to separate lines. At each position of the grid is either
+ * a space or a "#" character. They should form a chessboard when passed to a
+ * console.log statement.
+ */
+
