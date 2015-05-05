41d8f33a49ab68e01af08b62204fe6ad084de2a5
Josh Malkinson <josh.malkinson@gmail.com>
2015-05-05 09:38:41 +0100

    SumOfRange: add comment containing exercise

---
+++ 04_data/SumOfRange.js
@@ -0,0 +1,13 @@
+/* 
+ * Eloquent JavaScript, Chapter 4 Ex 1
+ *
+ * Write a range function that takes two arguments, start and end, and returns
+ * an array containing all the numbers from start up to (and including) end.
+ * Next write a sum function that takes an array of numbers and returns the sum
+ * of these numbers. Run `console.log(sum(range(1, 10))); and see if it returns
+ * 55.
+ *
+ * Extra credit: modify range function to take a `step` value which defaults to
+ * 1.
+ */
+
