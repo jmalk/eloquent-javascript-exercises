864eaf1666365f22557e7af2a305d8ea005293e5
Josh Malkinson <josh.malkinson@gmail.com>
2015-05-15 15:56:23 +0100

    ReverseAnArray: statement of problem

---
+++ 04_data/ReverseAnArray.js
@@ -0,0 +1,10 @@
+/* 
+ * Eloquent JavaScript, Chapter 4 Ex 2
+ *
+ * Write two functions, reverseArray and reverseArrayInPlace. reverseArray takes
+ * an array as argument and produces a new array that has the same elements in
+ * the inverse order. reverseArrayInPlace modifies and returns the array given
+ * as argument.
+ */
+
+
