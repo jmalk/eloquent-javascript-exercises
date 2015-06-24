faf6bc84141acfb220cf70f4ebbba30819668df5
Josh Malkinson <josh.malkinson@gmail.com>
2015-06-24 15:56:23 +0100

    ReverseAnArray: add reverse in place function

--- 04_data/ReverseAnArray.js
+++ 04_data/ReverseAnArray.js
@@ -20,4 +20,20 @@ var reverseArray = function(givenArray) {
 var abc = ['A', 'B', 'C'];
 console.log('Reversed array: ' + reverseArray(abc));
 
-//TODO add reverseArrayInPlace
+var reverseArrayInPlace = function(anArray) {
+    var lastElement = anArray.length - 1;
+    var midpoint = Math.floor(lastElement/2);
+    var temporaryVariable;
+    for(var i = 0; i <= midpoint; i += 1) {
+        temporaryVariable = anArray[i];
+        anArray[i] = anArray[lastElement - i];
+        anArray[lastElement - i] = temporaryVariable;
+    }
+    return anArray;
+};
+
+var abcde = ['A', 'B', 'C', 'D', 'E'];
+console.log('Reversed-in-place array: ' + reverseArrayInPlace(abcde));
+
+var numbers = [1, 2, 3, 4, 5, 6];
+console.log('Reversed-in-place numbers: ' + reverseArrayInPlace(numbers));
