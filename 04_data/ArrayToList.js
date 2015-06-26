77325dd4144a0e9e8bd95fa89b5abc5122eae9c1
Josh Malkinson <josh.malkinson@gmail.com>
2015-06-26 17:44:58 +0100

    Reformat comments, add prepend and nth functions

--- 04_data/ArrayToList.js
+++ 04_data/ArrayToList.js
@@ -1,20 +1,25 @@
 /*
- * Eloquent JavaScript, Chapter 4 Ex 3 A list is a nested set of objects, with
- * the first object holding a reference * to the second, the second to the
- * third, and so on.  Write a function arrayToList that builds up a data
- * structure like the previous one when given [1, 2, 3] as argument, and write
- * a listToArray function that produces an array from a list.  Also write the
- * helper functions prepend, which takes an element and a list and creates a
- * new list that adds the element to the front of the input list, and nth,
- * which takes a list and a number and returns the element at the given
- * position in the list, or undefined when there is no such element.
+ * Eloquent JavaScript, Chapter 4 Ex 3
  *
- * If you haven’t already, also write a recursive version of nth.
+ * A list is a nested set of objects, with the first object holding a reference
+ * to the second, the second to the third, and so on.
+ *
+ * Write a function arrayToList that builds up a data structure like the
+ * previous one when given [1, 2, 3] as argument, and write a listToArray
+ * function that produces an array from a list.
+ *
+ * Also write the helper functions prepend, which takes an element and a list
+ * and creates a new list that adds the element to the front of the input list,
+ * and nth, which takes a list and a number and returns the element at the
+ * given position in the list, or undefined when there is no such element.
+ *
+ * TODO: If you haven’t already, also write a recursive version of nth.
  */
 
 // Some tests
 
 var arrayToList = function(array) {
+    "use strict";
     var list = null;
     for (var i = array.length - 1; i >= 0; i -= 1) {
         list = {
@@ -26,6 +31,7 @@ var arrayToList = function(array) {
 };
 
 var listToArray = function(list) {
+    "use strict";
     var objectWeAreLookingAt = list;
     var arrayWeWillReturn = [];
     while (objectWeAreLookingAt !== null) {
@@ -35,11 +41,35 @@ var listToArray = function(list) {
     return arrayWeWillReturn;
 };
 
+var prepend = function(element, list) {
+    "use strict";
+    var newList = {
+        value: element,
+        rest: list
+    };
+    return newList;
+};
+
+var nth = function(list, index) {
+    "use strict";
+    var objectWeAreLookingAt = list;
+    var count = 0;
+    while (objectWeAreLookingAt !== null) {
+        if (count === index) {
+            return objectWeAreLookingAt.value;
+        }
+        count += 1;
+        objectWeAreLookingAt = objectWeAreLookingAt.rest;
+    }
+};
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
-// // → [10, 20, 30]
-// console.log(prepend(10, prepend(20, null)));
-// // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(nth(arrayToList([10, 20, 30]), 1));
-// // → 20
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+
+// TODO: Add meaningful expected/got console output, or a light test framework.
