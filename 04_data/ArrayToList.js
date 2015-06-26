4ec465fc3ac919f44de9ccfd7e0dad0f3ec5e061
Josh Malkinson <josh.malkinson@gmail.com>
2015-06-26 17:12:28 +0100

    Fix syntax errors

--- 04_data/ArrayToList.js
+++ 04_data/ArrayToList.js
@@ -1,13 +1,13 @@
 /*
- * Eloquent JavaScript, Chapter 4 Ex 3
- * A list is a nested set of objects, with the first object holding a reference * to the second, the second to the third, and so on.
- * Write a function arrayToList that builds up a data structure like the
- * previous one when given [1, 2, 3] as argument, and write a listToArray
- * function that produces an array from a list. Also write the helper functions
- * prepend, which takes an element and a list and creates a new list that adds
- * the element to the front of the input list, and nth, which takes a list and
- * a number and returns the element at the given position in the list, or
- * undefined when there is no such element.
+ * Eloquent JavaScript, Chapter 4 Ex 3 A list is a nested set of objects, with
+ * the first object holding a reference * to the second, the second to the
+ * third, and so on.  Write a function arrayToList that builds up a data
+ * structure like the previous one when given [1, 2, 3] as argument, and write
+ * a listToArray function that produces an array from a list.  Also write the
+ * helper functions prepend, which takes an element and a list and creates a
+ * new list that adds the element to the front of the input list, and nth,
+ * which takes a list and a number and returns the element at the given
+ * position in the list, or undefined when there is no such element.
  *
  * If you haven’t already, also write a recursive version of nth.
  */
@@ -28,12 +28,12 @@ var arrayToList = function(array) {
 var listToArray = function(list) {
     var objectWeAreLookingAt = list;
     var arrayWeWillReturn = [];
-    while (objectWeAreLookingAt != null) {
+    while (objectWeAreLookingAt !== null) {
         arrayWeWillReturn.push(objectWeAreLookingAt.value);
         objectWeAreLookingAt = objectWeAreLookingAt.rest;
     }
     return arrayWeWillReturn;
-}
+};
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
