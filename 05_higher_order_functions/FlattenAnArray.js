a02471ff58293faaabd3e2fa1df49d4edcb3fc3f
Josh Malkinson <josh.malkinson@gmail.com>
2015-07-08 13:52:21 +0100

    Flatten nested arrays into a 1-D array

---
+++ 05_higher_order_functions/FlattenAnArray.js
@@ -0,0 +1,13 @@
+/*
+ * Eloquent JavaScript, Chapter 5 Ex 1
+ *
+ * Use the reduce method in combination with the concat method to “flatten” an
+ * array of arrays into a single array that has all the elements of the input
+ * arrays.
+ */
+
+var arrays = [[1, 2, 3], [4, 5], [6]];
+var flattened = arrays.reduce(function(a, b) {
+    'use strict';
+    return a.concat(b); });
+console.log('Expected: [1, 2, 3, 4, 5, 6], got: ' + flattened);
