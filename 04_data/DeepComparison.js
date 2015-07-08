0de732d4c417f18db602e770e0bdb5f5b7dd26f1
Josh Malkinson <josh.malkinson@gmail.com>
2015-07-08 13:50:51 +0100

    Clean up syntax errors

--- 04_data/DeepComparison.js
+++ 04_data/DeepComparison.js
@@ -19,12 +19,37 @@
 
 var deepEqual = function(firstValue, secondValue) {
     "use strict";
-    if (firstValue === secondValue) {
-        return true;
+    if (typeof firstValue === "object" && firstValue !== null && typeof secondValue === "object" && secondValue !== null) {
+
+        var numberOfPropertiesInFirstObject = 0;
+        var numberOfPropertiesInSecondObject = 0;
+
+        for (var property in firstValue) {
+            if (firstValue.hasOwnProperty(property)) {
+            numberOfPropertiesInFirstObject += 1;
+        }
+
+        }
+        for (var property in secondValue) {
+                if (secondValue.hasOwnProperty(property)) {
+            numberOfPropertiesInSecondObject += 1;
+            }
+        }
+
+        if (numberOfPropertiesInFirstObject !== numberOfPropertiesInSecondObject) {
+            return false;
+        }
+    }
+    else {
+        return firstValue === secondValue;
     }
 };
 
 var obj = {here: {is: "an"}, object: 2};
+var obj2 = {This: "object", has: "many", properties: "of", its: "own"};
 console.log('Expect true, got ' + deepEqual(obj, obj));
 console.log('Expect false, got ' + deepEqual(obj, {here: 1, object: 2}));
 console.log('Expect true, got ' + deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log('Expect true, got ' + deepEqual(1, 1));
+console.log('Expect false, got ' + deepEqual(1, 2));
+console.log('Expect false, got ' + deepEqual(obj, obj2));
