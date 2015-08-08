1db45a2bc25251d4b619ae232b33205dadfb03b8
Josh Malkinson <josh.malkinson@gmail.com>
2015-08-08 21:47:19 +0100

    Implement function 'some'

--- 05_higher_order_functions/EveryAndThenSome.js
+++ 05_higher_order_functions/EveryAndThenSome.js
@@ -26,11 +26,22 @@ function every(array, qualifier) {
     return true;
 }
 
+function some(array, qualifier) {
+    'use strict';
+
+    for (var i = 0; i < array.length; i++) {
+        if (qualifier(array[i]) === true) {
+            return true;
+        }
+    }
+    return false;
+}
+
 console.log(every([NaN, NaN, NaN], isNaN));
 // → true
 console.log(every([NaN, NaN, 4], isNaN));
 // → false
-//console.log(some([NaN, 3, 4], isNaN));
+console.log(some([NaN, 3, 4], isNaN));
 // → true
-//console.log(some([2, 3, 4], isNaN));
+console.log(some([2, 3, 4], isNaN));
 // → false
