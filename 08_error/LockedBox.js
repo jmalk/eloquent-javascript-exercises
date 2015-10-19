1901cbcef064321b8014171ab72b979766dd4f72
Josh Malkinson <josh.malkinson@gmail.com>
2015-10-19 14:04:05 +0100

    Add starter code from book

--- 08_error/LockedBox.js
+++ 08_error/LockedBox.js
@@ -9,5 +9,40 @@
  * argument, unlocks the box, runs the function, and then ensures that the box
  * is locked again before returning, regardless of whether the argument
  * function returned normally or threw an exception. 
+ *
+ * For extra points, make sure that if you call withBoxUnlocked when the box is
+ * already unlocked, the box stays unlocked.
  */
 
+'use strict';
+
+var box = {
+    locked: true,
+    unlock: function() { this.locked = false; },
+    lock: function() { this.locked = true;  },
+    _content: [],
+    get content() {
+        if (this.locked) {
+            throw new Error("Locked!");
+        }
+        return this._content;
+    }
+};
+
+function withBoxUnlocked(body) {
+    // Your code here.
+}
+
+withBoxUnlocked(function() {
+    box.content.push("gold piece");
+});
+
+try {
+    withBoxUnlocked(function() {
+        throw new Error("Pirates on the horizon! Abort!");
+    });
+} catch (e) {
+    console.log("Error raised:", e);
+}
+console.log(box.locked);
+// → true
