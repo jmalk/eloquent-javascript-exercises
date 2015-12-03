7e9bf0e2ffd9fd7e556b4f51ee42f90ad4b9d128
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-03 16:00:39 +0000

    Add a critter that moves randomly

--- 07_elife/ElectronicLife.js
+++ 07_elife/ElectronicLife.js
@@ -57,3 +57,20 @@ var directions = {
     "nw": new Vector(-1, -1)
 };
 
+function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+var directionNames = "n ne e se s sw w nw".split(" ");
+
+function BouncingCritter() {
+    this.direction = randomElement(directionNames);
+}
+
+BouncingCritter.prototype.act = function(view) {
+    if (view.look(this.direction) !== " ") {
+        this.direction = view.find(" ") || "s";
+    }
+    return {type: "move", direction: this.direction};
+};
+
