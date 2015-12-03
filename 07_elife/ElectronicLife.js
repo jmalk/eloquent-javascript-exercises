838d447303c8e74fc4f3085833797ba78e1188d1
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-03 15:50:49 +0000

    Add cardinal directions

--- 07_elife/ElectronicLife.js
+++ 07_elife/ElectronicLife.js
@@ -45,3 +45,15 @@ var grid = new Grid(5, 5);
 console.log(grid.get(new Vector(1, 1)));
 grid.set(new Vector(1, 1), 'X');
 console.log(grid.get(new Vector(1, 1)));
+
+var directions = {
+    "n":  new Vector( 0, -1),
+    "ne": new Vector( 1, -1),
+    "e":  new Vector( 1,  0),
+    "se": new Vector( 1,  1),
+    "s":  new Vector( 0,  1),
+    "sw": new Vector(-1,  1),
+    "w":  new Vector(-1,  0),
+    "nw": new Vector(-1, -1)
+};
+
